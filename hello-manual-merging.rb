a1bbf5a765ef2ee1ec9078e642717eb7013738cd
Shreyas Taware <shreyas.taware@authbridge.com>
2021-12-02 23:40:55 +0530

    documenting hello function

--- hello-manual-merging.rb
+++ hello-manual-merging.rb
@@ -1,5 +1,6 @@
 #! /usr/bin/env ruby
 
+# greeting to open-source newbies and distributed VC freshers and also ruby enthusiasts
 def hello
 	puts 'hello to the world of distributed version control'
 end
